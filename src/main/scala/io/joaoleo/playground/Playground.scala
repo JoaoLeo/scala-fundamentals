6d1854f77087a85df51b2cc776f071acb1e5928b
João Leonardo <joaoleonardo9921@gmail.com>
2025-04-13 00:06:55 -0300

    binary search in scala

--- src/main/scala/io/joaoleo/playground/Playground.scala
+++ src/main/scala/io/joaoleo/playground/Playground.scala
@@ -1,7 +1,23 @@
 package io.joaoleo.playground
 
 object Playground {
+
+  def binarySearch(list: List[Int], item: Int): Option[Int] = {
+    var lowest = 0
+    var highest = list.length - 1
+
+    while (lowest <= highest)
+      val mid = lowest + (highest - lowest) / 2
+      val guess = list(mid)
+
+      if (guess == item) return Some(mid)
+      else if (guess > item) highest = mid - 1
+      else lowest = mid + 1
+    
+    None
+  }
+
   def main(args: Array[String]): Unit = {
-    println("Hello World!")
+    println(binarySearch(List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), 5))
   }
 }
